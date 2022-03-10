925363970d186004c8538416733f1b9cb17d5dea
TylerDurden16 <larrysellers16@gmail.com>
2022-03-10 08:24:02 -0800

    added JS exercises

---
+++ correctInput.js
@@ -0,0 +1,11 @@
+
+for (let i = 0; ; i++) {
+    const numberQuestion = window.prompt('Enter a number greater than 100');
+console.log(typeof numberQuestion);
+
+    if (numberQuestion < 100) {
+       
+    } if (numberQuestion >= 100 || numberQuestion  === null || numberQuestion === '') {
+        break;
+    }
+} 
